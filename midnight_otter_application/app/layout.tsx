7ba4fe006bc78fcc1f339537dfef279aaaa494a4
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-08-06 17:37:38 +0200

    Add metadata

--- midnight_otter_application/app/layout.tsx
+++ midnight_otter_application/app/layout.tsx
@@ -1,7 +1,8 @@
-"use client";
 
 import "./globals.css";
 import { Inter } from "next/font/google";
+
+import { siteConfig } from "@/config/siteConfig";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,10 +10,60 @@ interface DefaultLayoutProps {
   children: React.ReactNode;
 }
 
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "shadcn",
+      url: "https://shadcn.com",
+    },
+  ],
+  creator: "shadcn",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: "@shadcn",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
+
 export default function RootLayout({ children }: DefaultLayoutProps) {
   return (
-    <html lang="en" className="antialiased">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className="min-h-screen bg-background antialiased">
+        {children}
+      </body>
     </html>
   );
 }
