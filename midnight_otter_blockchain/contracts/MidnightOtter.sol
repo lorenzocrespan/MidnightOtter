46a02f8701a02582cb032c2bb133abf215d7fd1a
Lorenzo Crespan <lorenzo.crespan@gmail.com>
2023-11-17 11:41:05 +0100

    Add expert report field and get owner cases

--- midnight_otter_blockchain/contracts/MidnightOtter.sol
+++ midnight_otter_blockchain/contracts/MidnightOtter.sol
@@ -11,8 +11,11 @@ contract MidnightOtter is ERC721, ERC721Enumerable, AccessControl {
 
     /**
      *  @dev Structure of the property of the case.
-     *  
+     *
      *  @param numberCase Number of the case.
+     *  @param nameCase Name of the case.
+     *
+     *  @param expertReports List of URI for the expert reports.
      */
     struct Property {
         // Properties of the case
@@ -20,65 +23,82 @@ contract MidnightOtter is ERC721, ERC721Enumerable, AccessControl {
         string nameCase;
         // string nameCreator;
         // List of URI for the expert reports
-        // string[] expertReports;
+        string[] expertReports;
         // URI for the chain of custody
         // string chainCustody;
     }
 
     mapping(uint256 => Property) public caseProperties;
 
-    constructor(address defaultAdmin, address minter)
-        ERC721("MidnightOtter", "MOK")
-    {
+    constructor(
+        address defaultAdmin,
+        address minter
+    ) ERC721("MidnightOtter", "MOK") {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
         _grantRole(MINTER_ROLE, minter);
     }
 
-    function safeMint(address to, Property memory initialTokenStruct ) public onlyRole(MINTER_ROLE) {
+    function safeMint(
+        address to,
+        Property memory initialTokenStruct
+    ) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         caseProperties[tokenId] = initialTokenStruct;
     }
 
-        function showProperty(uint256 tokenId)
-        public
-        view
-        returns (
-            uint256,
-            string memory
-            // string memory,
-            // string[] memory,
-            // string memory
-        )
-    {
-        Property memory property = caseProperties[tokenId];
-        return (
-            property.numberCase,
-            property.nameCase
-            // property.nameCreator,
-            // property.expertReports,
-            // property.chainCustody
+    // The following functions are required to interact with the property of the case.
+
+    function getCaseProperties(
+        uint256 tokenId
+    ) public view returns (Property memory) {
+        return caseProperties[tokenId];
+    }
+
+    function getOwnerCases(
+        address owner
+    ) public view returns (uint256[] memory) {
+        uint256 balance = balanceOf(owner);
+        uint256[] memory cases = new uint256[](balance);
+        for (uint256 i = 0; i < balance; i++) {
+            cases[i] = tokenOfOwnerByIndex(owner, i);
+        }
+        return cases;
+    }
+
+    function addExpertReport(
+        uint256 tokenId,
+        string memory expertReport
+    ) public onlyRole(MINTER_ROLE) {
+        // Check if the sender is the owner of the case
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "MidnightOtter: Only the owner of the case can add an expert report."
         );
+        // Add the expert report to the list of expert reports
+        caseProperties[tokenId].expertReports.push(expertReport);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
         return super._update(to, tokenId, auth);
     }
 
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
         super._increaseBalance(account, value);
     }
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
         public
         view
         override(ERC721, ERC721Enumerable, AccessControl)
