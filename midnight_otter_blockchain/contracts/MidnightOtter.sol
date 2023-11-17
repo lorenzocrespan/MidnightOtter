877203d99b0bb0b0c7de130ee5ee0e424f2fc696
Lorenzo Crespan <lorenzo.crespan@gmail.com>
2023-11-17 11:32:42 +0100

    Add contract role and struct for chain of custody

--- midnight_otter_blockchain/contracts/MidnightOtter.sol
+++ midnight_otter_blockchain/contracts/MidnightOtter.sol
@@ -3,37 +3,69 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MidnightOtter is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Ownable {
+contract MidnightOtter is ERC721, ERC721Enumerable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 private _nextTokenId;
 
-    constructor(address initialOwner)
-        ERC721("MidnightOtter", "CMO")
-        Ownable(initialOwner)
-    {}
-
-    function pause() public onlyOwner {
-        _pause();
+    /**
+     *  @dev Structure of the property of the case.
+     *  
+     *  @param numberCase Number of the case.
+     */
+    struct Property {
+        // Properties of the case
+        uint256 numberCase;
+        string nameCase;
+        // string nameCreator;
+        // List of URI for the expert reports
+        // string[] expertReports;
+        // URI for the chain of custody
+        // string chainCustody;
     }
 
-    function unpause() public onlyOwner {
-        _unpause();
+    mapping(uint256 => Property) public caseProperties;
+
+    constructor(address defaultAdmin, address minter)
+        ERC721("MidnightOtter", "MOK")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, Property memory initialTokenStruct ) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        caseProperties[tokenId] = initialTokenStruct;
+    }
+
+        function showProperty(uint256 tokenId)
+        public
+        view
+        returns (
+            uint256,
+            string memory
+            // string memory,
+            // string[] memory,
+            // string memory
+        )
+    {
+        Property memory property = caseProperties[tokenId];
+        return (
+            property.numberCase,
+            property.nameCase
+            // property.nameCreator,
+            // property.expertReports,
+            // property.chainCustody
+        );
     }
 
     // The following functions are overrides required by Solidity.
 
     function _update(address to, uint256 tokenId, address auth)
         internal
-        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        override(ERC721, ERC721Enumerable)
         returns (address)
     {
         return super._update(to, tokenId, auth);
@@ -46,19 +78,10 @@ contract MidnightOtter is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Paus
         super._increaseBalance(account, value);
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        override(ERC721, ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
