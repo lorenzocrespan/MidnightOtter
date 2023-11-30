d1b95c013db41cf398074ad6ca7fd4a7facbbb70
Lorenzo Crespan <lorenzo.crespan@gmail.com>
2023-11-30 11:43:04 +0100

    Improve smart contract

--- midnight_otter_blockchain/contracts/MidnightOtter.sol
+++ midnight_otter_blockchain/contracts/MidnightOtter.sol
@@ -8,30 +8,87 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract MidnightOtter is ERC721, ERC721Enumerable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 private _nextTokenId;
-
     /**
-     *  @dev Structure of the property of the case.
-     *
-     *  @param numberCase Number of the case.
-     *  @param nameCase Name of the case.
-     *
-     *  @param expertReports List of URI for the expert reports.
+     * @notice Enum of the state of the case.
      */
-    struct Property {
-        // Properties of the case
-        uint256 numberCase;
-        string nameCase;
-        // string nameCreator;
-        // Properties of object of the case
-        string nameObject;
-        string descriptionObject;
-        // List of URI for the expert reports
-        string[] expertReports;
-        // URI for the chain of custody
-        // string chainCustody;
+    enum CaseStateValue {
+        Open,
+        Closed
+    }
+    /**
+     * @notice Enum of the status of the object.
+     */
+    enum ObjectStatusValue {
+        Seized,
+        Released,
+        Destroyed
     }
 
-    mapping(uint256 => Property) public caseProperties;
+    /**
+     *  @dev Structure of the exihibit of the case.
+     *
+     *  @param caseNumber Number of the case.
+     *  @param caseName Name of the case.
+     *  @param caseState State of the case.
+     *
+     *  @param submitterOfficer Name of the submitting officer.
+     *  @param submitterId Id of the submitter officer.
+     *  @param objectId Id of the object.
+     *  @param objectQuantity Quantity of the object.
+     *  @param objectDescription Description of the object.
+     *  @param seizedLocation Location of the seizure.
+     *  @param seizedEpochTime Epoch time of the seizure.
+     *  @param expertReports List of URI for the expert reports.
+     *
+     *  @param objectStatus Status of the object (Seized, Released, Destroyed).
+     *
+     *  @param chainCustody List of events for the chain of custody.
+     */
+    struct Exihibit {
+        // Properties of the case
+        uint256 caseNumber;
+        string caseName;
+        CaseStateValue caseState;
+        // Properties of the submitter officer
+        string submitterOfficer;
+        uint256 submitterId;
+        // Properties of the object
+        uint256 objectId;
+        uint256 objectQuantity;
+        string objectDescription;
+        // Properties of the seizure
+        string seizedLocation;
+        uint256 seizedEpochTime;
+        // Properties of the object status
+        ObjectStatusValue objectStatus;
+        // List of URI for the expert reports
+        string[] expertReports;
+        // List of events for the chain of custody
+        ChainCustody[] chainCustody;
+    }
+
+    /**
+     *  @dev Structure of the chain of custody.
+     *
+     *  @param timestamp Timestamp of the event.
+     *  @param releasedBy Name of the officer who released the object.
+     *  @param receivedBy Name of the officer who received the object.
+     *  @param action Action of the event.
+     */
+    struct ChainCustody {
+        uint256 timestamp;
+        string releasedBy;
+        string receivedBy;
+        string action;
+    }
+
+    // Mapping of the properties of the exihibit of the case
+    // Given a token id, return the properties of the case
+    mapping(uint256 => Exihibit) public caseProperties;
+
+    // Mapping of the list of users who have access to the exihibit of the case
+    // Given a token id, return the list of users
+    mapping(uint256 => address[]) public sharedExihibit;
 
     constructor(
         address defaultAdmin,
@@ -43,32 +100,28 @@ contract MidnightOtter is ERC721, ERC721Enumerable, AccessControl {
 
     function safeMint(
         address to,
-        Property memory initialTokenStruct
+        Exihibit memory initialTokenStruct
     ) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         caseProperties[tokenId] = initialTokenStruct;
     }
 
-    function unsafeMint(
-        address to,
-        Property memory initialTokenStruct
-    ) public {
-         uint256 tokenId = _nextTokenId++;
-         _mint(to, tokenId);
+    function unsafeMint(address to, Exihibit memory initialTokenStruct) public {
+        uint256 tokenId = _nextTokenId++;
+        _mint(to, tokenId);
         caseProperties[tokenId] = initialTokenStruct;
     }
-
 
     function getActualTokenId() public view returns (uint256) {
         return _nextTokenId;
     }
 
-    // The following functions are required to interact with the property of the case.
+    // The following functions are required to interact with the Exihibit of the case.
 
     function getCaseProperties(
         uint256 tokenId
-    ) public view returns (Property memory) {
+    ) public view returns (Exihibit memory) {
         return caseProperties[tokenId];
     }
 
@@ -83,18 +136,94 @@ contract MidnightOtter is ERC721, ERC721Enumerable, AccessControl {
         return cases;
     }
 
-    function addExpertReport(
+    // The following functions are required to manage the sharing of the Exihibit of the case.
+
+    function grantShareExihibit(
         uint256 tokenId,
-        string memory expertReport
+        address user
     ) public onlyRole(MINTER_ROLE) {
         // Check if the sender is the owner of the case
         require(
             ownerOf(tokenId) == msg.sender,
+            "MidnightOtter: Only the owner of the case can share the exihibit."
+        );
+        // Check if the user is already in the list of users
+        for (uint256 i = 0; i < sharedExihibit[tokenId].length; i++) {
+            require(
+                sharedExihibit[tokenId][i] != user,
+                "MidnightOtter: The user is already in the list of users."
+            );
+        }
+        // Add the user to the list of users
+        sharedExihibit[tokenId].push(user);
+    }
+
+    function ungrantShareExihibit(
+        uint256 tokenId,
+        address user
+    ) public onlyRole(MINTER_ROLE) {
+        // Check if the sender is the owner of the case
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "MidnightOtter: Only the owner of the case can unshare the exihibit."
+        );
+        // Check if the user is in the list of users
+        bool isUserInList = false;
+        for (uint256 i = 0; i < sharedExihibit[tokenId].length; i++) {
+            if (sharedExihibit[tokenId][i] == user) {
+                isUserInList = true;
+            }
+        }
+        require(
+            isUserInList,
+            "MidnightOtter: The user is not in the list of users."
+        );
+        // Remove the user from the list of users
+        for (uint256 i = 0; i < sharedExihibit[tokenId].length; i++) {
+            if (sharedExihibit[tokenId][i] == user) {
+                delete sharedExihibit[tokenId][i];
+            }
+        }
+    }
+
+    function unshareExihibit(uint256 tokenId) public onlyRole(MINTER_ROLE) {
+        // Check if the sender is the owner of the case
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "MidnightOtter: Only the owner of the case can unshare the exihibit."
+        );
+        // Remove all the users from the list of users
+        for (uint256 i = 0; i < sharedExihibit[tokenId].length; i++) {
+            delete sharedExihibit[tokenId][i];
+        }
+    }
+
+    function shareOf(uint256 tokenId, address user) public view returns (bool) {
+        // Check if the user is in the list of users
+        for (uint256 i = 0; i < sharedExihibit[tokenId].length; i++) {
+            if (sharedExihibit[tokenId][i] == user) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // The following functions are required to manage the expert reports of the Exihibit of the case.
+
+    function addExpertReport(
+        uint256 tokenId,
+        string memory expertReport
+    ) public onlyRole(MINTER_ROLE) {
+        // Check if the sender is the owner of the exihibit or a shared user
+        require(
+            ownerOf(tokenId) == msg.sender || shareOf(tokenId, msg.sender),
             "MidnightOtter: Only the owner of the case can add an expert report."
         );
         // Add the expert report to the list of expert reports
         caseProperties[tokenId].expertReports.push(expertReport);
     }
+
+    // The following functions are required to manage the chain of custody of the Exihibit of the case.
 
     // The following functions are overrides required by Solidity.
 
