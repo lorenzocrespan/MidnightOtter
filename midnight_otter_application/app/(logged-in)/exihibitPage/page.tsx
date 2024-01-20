98da0790d76a631b22b0e9b1d135dc01d8100bf8
Lorenzo Crespan <lorenzo.crespan@gmail.com>
2024-01-20 15:34:19 +0100

    Refactor exihibit list

--- midnight_otter_application/app/(logged-in)/exihibitPage/page.tsx
+++ midnight_otter_application/app/(logged-in)/exihibitPage/page.tsx
@@ -87,35 +87,56 @@ export default function Page() {
   return (
     <div className="flex h-auto flex-col justify-between gap-5 rounded-md p-12">
       <div className="flex flex-col gap-2">
-        <h1 className="text-3xl font-bold">Lista casi</h1>
-        <p className="text-base"> Seleziona il caso da visualizzare.</p>
+        <h1 className="text-3xl font-bold">Prove a carico</h1>
+        <p className="text-base">
+          Lista delle prove attualmente a carico dell'utente.
+        </p>
       </div>
       <div className="flex flex-col gap-5">
         {Object.keys(groupedExihibitsCases).map((keyName: any, i: any) => (
           <div className="flex flex-col gap-5 p-4" key={i}>
-            <h1 className="flex text-3xl font-bold">{keyName}</h1>
-            <span className="w-full border-t border-slate-600" />
+            <h1 className="flex text-3xl font-bold">Caso {keyName}</h1>
+            <span className="w-full border-t border-slate-400" />
             <div className="flex flex-col items-center gap-4">
               {groupedExihibitsCases[keyName].map((item: any, idx: any) => (
-                <div key={idx} className="flex w-full gap-5 text-lg">
-                  <div className="flex flex-col p-2">
-                    <h2 className="font-bold">
-                      Identificativo: {Number(item.exhibitInformation.objectId)}
-                    </h2>
-                    <p className="text-base">
-                      {item.exhibitInformation.objectDescription}
-                    </p>
-                  </div>
-                  <div className="flex grow" />
-                  <div className="flex flex-col justify-center">
-                    <Link
-                      href={`/exihibitPage/${Number(
-                        item.exhibitInformation.objectId
-                      )}`}
+                <div key={idx} className="flex w-11/12  flex-col gap-2">
+                  <div className="flex gap-5 text-lg">
+                    <div className="flex flex-col p-2">
+                      <h2 className="font-bold">
+                        Identificativo prova:{" "}
+                        {Number(item.exhibitInformation.objectId)}
+                      </h2>
+                      <p className="text-base">
+                        {item.exhibitInformation.objectDescription}
+                      </p>
+                    </div>
+                    <div className="flex grow" />
+                    <div
+                      className={
+                        "h-fit w-24 select-none self-center rounded-full px-2 py-1 text-center text-base font-bold text-gray-900 " +
+                        (item.exhibitInformation.isExihibit
+                          ? "bg-green-700"
+                          : "bg-red-700")
+                      }
+                      title="Prova o Reperto"
                     >
-                      <ChevronRight size={45} />
-                    </Link>
+                      <span>
+                        {item.exhibitInformation.isExihibit
+                          ? "Prova"
+                          : "Reperto"}
+                      </span>
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <Link
+                        href={`/exihibitPage/${Number(
+                          item.exhibitInformation.objectId
+                        )}`}
+                      >
+                        <ChevronRight size={45} />
+                      </Link>
+                    </div>
                   </div>
+                  <span className="h-px w-full bg-gradient-to-r from-slate-500 to-black" />
                 </div>
               ))}
             </div>
